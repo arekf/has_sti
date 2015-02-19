76cffdbd6aa41b568bda87e1345cd38e2a098c3c
Arkadiusz Fal <arek@arekf.net>
2015-02-19 20:42:16 +0100

    Add tests for scopes

--- spec/dummy/spec/models/animal_spec.rb
+++ spec/dummy/spec/models/animal_spec.rb
@@ -28,4 +28,41 @@ RSpec.describe Animal, :type => :model do
   it 'should not be a zebra' do
     expect(@animal).not_to be_zebra
   end
+
+  describe 'scope' do
+    before do
+      @nice_cat = Cat.create
+      @sleepy_cat = Cat.create
+
+      @cute_elephant = Elephant.create
+      @helpful_elephant = Elephant.create
+
+      @beautiful_zebra = Zebra.create
+      @awesome_zebra = Zebra.create
+    end
+
+    it 'should respond to cat scope' do
+      expect(Animal).to respond_to(:cat)
+    end
+
+    it 'should respond to elephant scope' do
+      expect(Animal).to respond_to(:elephant)
+    end
+
+    it 'should respond to zebra scope' do
+      expect(Animal).to respond_to(:zebra)
+    end
+
+    it 'should return cats' do
+      expect(Animal.cat).to match_array [@nice_cat, @sleepy_cat]
+    end
+
+    it 'should return elephants' do
+      expect(Animal.elephant).to match_array [@cute_elephant, @helpful_elephant]
+    end
+
+    it 'should return zebras' do
+      expect(Animal.zebra).to match_array [@beautiful_zebra, @awesome_zebra]
+    end
+  end
 end
